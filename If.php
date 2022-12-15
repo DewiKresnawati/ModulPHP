9f4583601407b2240ec44e9ca17826c45841a3b8
DewiKresnawati <dewidesember20@gmail.com>
2022-12-15 10:59:32 +0700

    ModulPHP

---
+++ If.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>if</title>
+    </head>
+    <body>
+        <h2> Penggunaan Kontrol if </h2>
+        <?php
+        $Umur=18;
+        echo ("umur anda $umur tahun <br>");
+        if ($umur>=17)
+        {
+            echo ("Anda Boleh Masuk");
+        }
+        ?>
+    </body>
+</html>
---
+++ coba.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uji coba</title>
+</head>
+<body>
+    <h1>Hello Word!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse quas tempora inventore minus a modi facilis, quia earum at autem recusandae provident dolor qui culpa numquam deserunt quam aliquam sit.</p>
+
+    <!-- memasukan php disini -->
+    <?php
+$txt = "PHP";
+echo "I love $txt!";
+?>
+
+<?php
+echo "PHP code goes here"
+?>
+<!-- nambah -->
+<?php
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+?>
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+?>
+<br>
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?>
+</body>
+</html>
